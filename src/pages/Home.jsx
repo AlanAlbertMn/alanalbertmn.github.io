71fba46bbd049765206aeae014672fe6f582ca2d
Alan Rodrigo Albert Moran <alanalbertmn@gmail.com>
2025-10-20 00:28:59 -0600

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div className='bg-dark'>
+
+            {/*Theme Toogle*/}
+
+            {/*Background Effects*/}
+
+            {/*NavBar*/}
+
+            {/*Main Content*/}
+
+            {/*Footer*/}
+
+        </div>
+    )
+}
